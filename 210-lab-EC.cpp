085f18d99057fa67d2f96c74fff7886178442705
Majd-Bohsali <118876575+Majd-Bohsali@users.noreply.github.com>
2025-12-09 17:48:39 -0800

    populating map

--- 210-lab-EC.cpp
+++ 210-lab-EC.cpp
@@ -1,7 +1,28 @@
 // COMSC210 | Lab EC2 | Majd Bohsali
 // IDE Used: Visual Studio Code
 #include <iostream> 
+#include <map>
+#include <vector>
+#include <fstream>
+#include <string>
+using namespace std;
 
 int main() { 
+    ifstream inputFile("210-xc2-FA25.txt"); 
+    string mTitle;
+    int mRating; 
+    map<string, vector<int>> movieRatings;  
+
+    // Milestone 1: reads input file and populates the map 
+    if(inputFile.is_open()) { 
+        while(inputFile >> mTitle >> mRating) { 
+            movieRatings[mTitle].push_back(mRating); 
+        }
+        inputFile.close(); 
+    } else {
+        cout << "File could not open"; 
+    }
+
+
     return 0; 
 }
